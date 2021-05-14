8c41fd72af72b5cc7989d5bd9866b73c261a0f15
ahyoung <yoo@yanadoocorp.com>
2021-05-14 10:25:45 +0900

    yooay_20210514_탭간격수정_및_육각형썸네일_작업

--- src/assets/scripts/common.js
+++ src/assets/scripts/common.js
@@ -4,25 +4,24 @@
 const tabItem = document.querySelectorAll('.tab-item');
 const tabContent = document.querySelectorAll('.tab-content');
 
-Array.prototype.forEach.call(tabItem, e => {
-    e.addEventListener('click', tabEvent);
-})
+Array.prototype.forEach.call(tabItem, (e) => {
+  e.addEventListener('click', tabEvent);
+});
 
 function tabEvent(e) {
-    const tabTarget = e.currentTarget.parentNode;
-    const tabData = tabTarget.dataset.tab;
+  const tabTarget = e.currentTarget.parentNode;
+  const tabData = tabTarget.dataset.tab;
 
-    Array.prototype.forEach.call(tabItem, e => {
-        e.parentNode.classList.remove('active');
-    })
-    
-    Array.prototype.forEach.call(tabContent, e => {
-        e.classList.remove('active');
-    })
+  Array.prototype.forEach.call(tabItem, (e) => {
+    e.parentNode.classList.remove('active');
+  });
 
-    tabTarget.classList.add('active');
-    document.querySelector('#' + tabData).classList.add('active');
+  Array.prototype.forEach.call(tabContent, (e) => {
+    e.classList.remove('active');
+  });
 
+  tabTarget.classList.add('active');
+  document.querySelector('#' + tabData).classList.add('active');
 }
 
 // Nav
@@ -31,30 +30,30 @@ const delta = 5;
 const fixBox = document.querySelector('nav');
 const fixBoxHeight = fixBox.offsetHeight;
 let didScroll;
-window.onscroll = function(e) {
-    didScroll = true;
+window.onscroll = function (e) {
+  didScroll = true;
 };
 
-setInterval(function(){
-    if(didScroll){
-        hasScrolled();
-        didScroll = false;
-    }
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 250);
 
-function hasScrolled(){
-    let nowScrollTop = window.scrollY;
-    if(Math.abs(lastScrollTop - nowScrollTop) <= delta){
-        return;
+function hasScrolled() {
+  let nowScrollTop = window.scrollY;
+  if (Math.abs(lastScrollTop - nowScrollTop) <= delta) {
+    return;
+  }
+  if (nowScrollTop > lastScrollTop && nowScrollTop > fixBoxHeight) {
+    //Scroll down
+    fixBox.classList.remove('active');
+  } else {
+    if (nowScrollTop + window.innerHeight < document.body.offsetHeight) {
+      //Scroll up
+      fixBox.classList.add('active');
     }
-    if(nowScrollTop > lastScrollTop && nowScrollTop > fixBoxHeight){
-        //Scroll down
-        fixBox.classList.remove('active');
-    } else {
-        if(nowScrollTop + window.innerHeight < document.body.offsetHeight){
-            //Scroll up
-            fixBox.classList.add('active');
-        }
-    }
-    lastScrollTop = nowScrollTop;
+  }
+  lastScrollTop = nowScrollTop;
 }
